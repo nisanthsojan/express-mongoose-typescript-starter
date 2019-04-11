3445543ae173ebb4aac9e6b8ecb1cf8659234f19
Nisanth Sojan <nisanth.sojan@dkit.ie>
2019-04-11 15:40:13 +0100

    fix watch-debug command

--- copyStaticAssets.ts
+++ copyStaticAssets.ts
@@ -1,10 +1,12 @@
 import * as shell from "shelljs";
 
+shell.mkdir("-p", "dist");
 shell.cp("-f", "./package*.json", "dist/");
+shell.mkdir("-p", "dist/public");
 // Images
-shell.cp("-R", "src-public/images", "dist/public/");
+shell.cp("-rf", "src-public/images", "dist/public/images");
 // Fonts
 shell.mkdir("-p", "dist/public/fonts");
-shell.cp("-R", "src-public/fonts/Playfair_Display", "dist/public/fonts/Playfair_Display");
-shell.cp("-R", "node_modules/@fortawesome/fontawesome-free/webfonts", "dist/public/fonts/fontawesome-free");
+shell.cp("-rf", "src-public/fonts/Playfair_Display", "dist/public/fonts/Playfair_Display");
+shell.cp("-rf", "node_modules/@fortawesome/fontawesome-free/webfonts", "dist/public/fonts/fontawesome-free");
 
